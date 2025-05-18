2b0ebea75ecaaa171c2ae646e14051df7422189b
lubosq <lucas.bosq@epfedu.fr>
2025-05-18 16:02:27 +0200

    modification visualisation photos produits

--- app/src/main/java/com/example/ecommerceapp/ui/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/ui/ProductDetailsActivity.kt
@@ -22,6 +22,7 @@ class ProductDetailsActivity : AppCompatActivity() {
         product?.let {
             binding.tvProductTitle.text = it.title
             binding.tvProductPrice.text = "${it.price} €"
+            binding.tvProductRating.text = "Note : ${it.rating.rate} (${it.rating.count} avis)"
             binding.tvProductDescription.text = it.description
 
             Glide.with(this)
